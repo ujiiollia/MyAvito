1734b9e5f738ca609b8daa10c51ce4eca3ce5fd7
iLja <evilva00@gmail.com>
2024-04-06 18:49:10 +0300

    logger is apear

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,13 +3,42 @@ package main
 import (
 	"app/internal/config"
 	"fmt"
+	"log/slog"
+	"os"
 )
 
 func main() {
 	// config
 	cfg := config.MustLoad()
 	fmt.Println(cfg.Address)
-	//todo log
+	// log
+	log := setupLogger(cfg.Env)
+	log.Info("starting application ", slog.String("env", cfg.Env))
 	//todo logic
 	//todo start serv
+}
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log slog.Logger
+	switch env {
+	case envLocal:
+		log = *slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = *slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = *slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return &log
 }
